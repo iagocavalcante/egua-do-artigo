ea641c4fadb0b765586023b3681d35e48468dbf2
Iago Cavalcante <iagoangelimc@gmail.com>
2019-01-20 20:50:52 -0300

    Primeiro commit

---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <main class="article" v-html="html">
+    </main>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    paragraphs: Array
+  },
+  data: () => ({
+    html: '',
+  }),
+  mounted () {
+    this.html = this.transformArrayInHtml()
+  },
+  methods: {
+    transformArrayInHtml () {
+      return this.paragraphs.reduce((html, paragraph) => {
+        return html += `${paragraph.tag.replace('_$_', paragraph.text)}`
+      }, '')
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
 
 new Vue({
